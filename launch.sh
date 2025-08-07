8f3d46b42ee37ddbb5488994af6dc0f925280020
justintechnerd <223197036+justintechnerd@users.noreply.github.com>
2025-08-07 13:58:51 -0700

    Configuration for macOS compatibility

--- launch.sh
+++ launch.sh
@@ -17,6 +17,9 @@ elif [ -n "$WAYLAND_DISPLAY" ]; then
 elif [ -n "$DISPLAY" ]; then
     echo "Detected X11 environment"
     export SDL_VIDEODRIVER=x11
+elif [ -d "/Library/Apple" ]; then
+    echo "Detected macOS environment"
+    export SDL_VIDEODRIVER=cocoa
 else
     echo "No display server detected, trying framebuffer"
     export SDL_VIDEODRIVER=fbcon
